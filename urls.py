41b5eb70b4b7df24e6f878cff4e2bcd70a5e2d37
Dhanunjay-gurram <dhanunjaygurram12340@gmail.com>
2024-04-16 16:32:26 +0530

    3

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from SAMS.views import CustomLoginView
 
 urlpatterns = [
-    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     # Other URL patterns for your app...
 ]
